13cd59ac143b3b5197918c01a60c3bd2a9bc13f3
Klan <michaelsanderklan@gmail.com>
2020-12-16 16:03:22 +0100

    16/12 update

--- Projekt/BodyCheckModule.py
+++ Projekt/BodyCheckModule.py
@@ -1,85 +1,85 @@
 import sys
 import pandas as pd
-import whois
+import whois #maybe pip installed? in which case: pythonwhois
+w = whois.whois("pythonforbeginners.com")
 import socket, ssl
 import OpenSSL
+import re
+import requests
 from datetime import date, datetime
-from googlesearch import search
-from bs4 import BeautifulSoup
+from googlesearch import search #pip installed
+from bs4 import BeautifulSoup, SoupStrainer #pip installed
 import bs4
+# from urllib2 import
 
-today = date.today()
-
+realURL = "dr.dk"
 fakeURL = "https://www.fakewebsite.org/should_not_be_suspicious"
 fakePhishing = "http://www.gimmeyourinfo.ru.dk.com/@/add_something_suspicious//as_i_go_along.dk"
 
-#url = fakePhishing
 
-def Check_having_IP_Address(url):
+def CheckURLforPhishing(p_url):
+    # test_url=p_url
+    today = date.today()
+    url = p_url.lower()
+    
+
+
+    #url = fakePhishing
+    res = [0]*31
+
+# having_IP_Address  { -1,1 }
     #fjern https, http, etc og www og check om det indeholder tal med punktummer
-    return 1
-
-def Check_URL_Length(url):
+    res[0] = 1
+# URL_Length   { 1,0,-1 }
     if (len(url)<54):
-        return -1
+        res[1] = -1
     elif (len(url)>54 and len(url)<75):
-        return 0
+        res[1] = 0
     else: 
-        return 1
-    
-def Check_Shortining_Service(url):
-    lowercase = url
-    if (lowercase.count("bitly" or "tinyurl" or "ow.ly" or "rebrandly" or "t2m" or "clickmeter")>0):
-        return 1
+        res[1] = 1
+# Shortining_Service { 1,-1 }
+    if (url.count("bitly" or "tinyurl" or "ow.ly" or "rebrandly" or "t2m" or "clickmeter")>0):
+        res[2] = 1
     else:
-        return 0
-
-def Check_Having_At_symbol(url):
+        res[2] = 0
+# having_At_Symbol   { 1,-1 }
     if (url.count("@") > 0):
-        return 1
+        res[3] = 1
     else:
-        return 0
-    
-def Check_double_slash_redirecting(url):
+        res[3] = 0
+# double_slash_redirecting { -1,1 }
     doubleslash = url
     doubleslash.replace("https://", "")
     doubleslash.replace("http://", "")
     if (doubleslash.count("//")>0):
-        return 1
+        res[4] = 1
     else:
-        return 0
-    
-def Check_Prefix_Suffix(url):
+        res[4] = 0
+# Prefix_Suffix  { -1,1 }
     if (url.count("-") > 0):
-          return 1
+          res[5] = 1
     else:
 
-        return 0
-
-def Check_having_Sub_Domain(url):
+        res[5] = 0
+# having_Sub_Domain  { -1,0,1 }
     if (url.count(".") >= 1):
-        return -1
+        res[6] = -1
     elif (url.count(".") == 2):
-        return 0
+        res[6] = 0
     else: 
-        return 1
-    
-def Check_SSLfinal_State(url):
-        return 0
-    
-def Check_Domain_registeration_length(url):
-    w = whois.whois("pythonforbeginners.com")
-    print(w.updated_date[0].strftime("%Y-%m-%d"))
+        res[6] = 1
+# SSLfinal_State  { -1,1,0 }
+        res[7] = 0
+# Domain_registeration_length { -1,1 }
+    # print(w.updated_date[0].strftime("%Y-%m-%d"))
   #  print(w.updated_date[0])
-    print("crackhead")
-    print(today.strftime("%Y-%m-%d"))
-    print((w.updated_date[0] - w.creation_date[0]).days)
+    # print("crackhead")
+    # print(today.strftime("%Y-%m-%d"))
+    # print((w.updated_date[0] - w.creation_date[0]).days)
     
     #registrar, updated_url, creation_date, expiration_date, name servers
-    return 0
-    
-def Check_Favicon(url):
-
+    res[8] = 0
+# Favicon { 1,-1 }
     f1 = '<html><head><link rel="shortcut icon" href="https://xx.dk/favicon.ico" /></head><body>Test</body></html>'
     f2 = '<link rel="shortcut icon" href="favicon.ico" /><link rel="stylesheet" href="x.css" />'
     f3 = '<link rel="shortcut icon" href="./favicon.ico" />'
@@ -95,74 +95,104 @@ def Check_Favicon(url):
     for a in soup.find_all('link', href=True):
         if ("favicon" in str(a)):
             first_split = a['href'].split("//")
-            print(1, first_split)
+            # print(1, first_split)
             faviconreturn = 1
             if ("http" in first_split[0]):
-                print(2, 'has http', first_split[1])
+                # print(2, 'has http', first_split[1])
                 second_split = first_split[1].split("/")
-                print("3 host=", second_split[0])
+                # print("3 host=", second_split[0])
                 faviconreturn = 0
                 if (host == second_split[0]):
-                    print(4, "local on host", -1)
+                    # print(4, "local on host", -1)
                     faviconreturn = -1
                 else:
-                    print(5, "external host", 1, ("gottem"))
+                    # print(5, "external host", 1, ("gottem"))
                     faviconreturn = 1
             elif (first_split[0][0] == "."):
-                print(6, "local on host", -1)
+                # print(6, "local on host", -1)
                 faviconreturn = -1
             else:
-                print(7, "local on host", -1)
-    return faviconreturn
-            
-    
-def Check_port(url):
-        return 0
-    
-def Check_HTTPS_token(url):
+                # print(7, "local on host", -1)
+                faviconreturn = -1
+    res[9] = faviconreturn
+# port { 1,-1 }
+    res[10] = 0
+# HTTPS_token { -1,1 }
     nohttpstoken = url.split("//", 1)[1]
     if (nohttpstoken.count("https")>0):
-        return 1
+        res[11] = 1
     else:
-        return 0
+        res[11] = 0
+# Request_URL  { 1,-1 }
+    r_url = "http://stackoverflow.com/"
+    # r_url = "https://dr.dk/"
+
+    page = requests.get(r_url)
+    data = page.text
+    soup = BeautifulSoup(data, features="lxml")
+
+    successrate = 0
+    j = 0
+    domain = 'cdn.sstatic.net'
+    print("https://" + domain)
+    for img in soup.find_all('img', src=True):
+        print(img["src"], "https://" + domain in img['src'])
+        if ("https://" + domain in img['src'] or "http://" + domain in img['src']):
+            successrate += 1
+            # print(successrate)
+        j += 1
+    for audio in soup.find_all('audio', src=True):
+        print(img["src"], "https://" + domain in img['src'])
+        if ("https://" + domain in audio['src'] or "http://" + domain in audio['src']):
+            successrate += 1
+            # print(successrate)
+        j += 1
+    for embed in soup.find_all('embed', src=True):
+        print(img["src"], "https://" + domain in img['src'])
+        if ("https://" + domain in embed['src'] or "http://" + domain in embed['src']):
+            successrate += 1
+            # print(successrate)
+        j += 1
     
-def Check_Request_URL(url):
-        return 0
-    
-def Check_URL_of_Anchor(url):
-        return 0
-    
-def Check_Links_in_tags(url):
-        return 0
-    
-def Check_SFH(url):
-        return 0
-    
-def Check_Submitting_to_email(url):
-        return 0
-    
-def Check_Abnormal_URL(url):
-        
-        return 0
-    
-def Check_Redirect(url):
-        return 0
-    
-def Check_on_mouseover(url):
-        return 0
-    
-def Check_RightClick(url):
-        return 0
+    print(successrate)
+    print(j)
     
     
-    #their spelling error, not mine
-def Check_popUpWindow(url):
-        return 0
     
-def Check_Iframe(url):
-        return 0
-    
-def Check_age_of_domain(url):
+    try:
+        percentage = successrate / float(j) * 100
+        print(percentage)
+    except:
+        return 1
+
+    if percentage < 22.0:
+        res[12] = 1
+    elif 22.0 <= percentage < 61.0:
+        res[12] = 0
+    else:
+        res[12] = -1
+    print(res[12])
+# URL_of_Anchor { -1,0,1 }
+    res[13] = 0
+# Links_in_tags { 1,-1,0 }
+    res[14] = 0
+# SFH  { -1,1,0 }
+    res[15] = 0
+# Submitting_to_email { -1,1 }
+    res[16] = 0
+# Abnormal_URL { -1,1 }
+    res[17] = 0
+# Redirect  { 0,1 }
+    res[18] = 0
+# on_mouseover  { 1,-1 }
+    res[19] = 0
+# RightClick  { 1,-1 }
+    res[20] = 0
+# popUpWindow  { 1,-1 }
+    res[21] = 0
+# Iframe { 1,-1 }
+    res[22] = 0
+# age_of_domain  { -1,1 }
     hostname='online.carnegie.dk'
     #whois
     port=443
@@ -171,110 +201,38 @@ def Check_age_of_domain(url):
     x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
     pk = x509.get_pubkey()
 
-    print("Issuer:", x509.get_issuer())
-    print("Subject:", x509.get_subject())
-    print("Cert valid from:", datetime.strptime(x509.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ'))
-    print("Cert valid to:", datetime.strptime(x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'))
-    return 0
+    # print("Issuer:", x509.get_issuer())
+    # print("Subject:", x509.get_subject())
+    # print("Cert valid from:", datetime.strptime(x509.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ'))
+    # print("Cert valid to:", datetime.strptime(x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'))
+    res[23] = 0
+# DNSRecord   { -1,1 }
+    try:
+        print(socket.getaddrinfo(url,80))
+    except:
+        res[24] = 1
+        
+    # print("lookie here"),
+    # print(url)
+    # res[24] = 0
+# web_traffic  { -1,0,1 }
+    res[25] = 0
+# Page_Rank { -1,1 }
+    res[26] = 0
+# Google_Index { 1,-1 }
+    site = search(url,5)
+    if(site):
+        res[27] = 1
+    else: res[27] = -1
+# Links_pointing_to_page { 1,0,-1 }
+    res[28] = 0
+# Statistical_report { -1,1 }
+    res[29] = 0
+# Result  { -1,1 }
+    res[30] = 0
+    return res
     
-def Check_DNSRecord(url):
-        return 0
-    
-def Check_web_traffic(url):
-        return 0
-    
-def Check_Page_Rank(url):
-        return 0
-    
-def Check_Google_Index(url):
-        site = search(url,5)
-        return 1 if site else -1
-    
-def Check_Links_pointing_to_page(url):
-        return 0
-    
-def Check_Statistical_report(url):
-        return 0
-    
-def Check_Result(url):
-        return 0
+print(CheckURLforPhishing(fakePhishing))
 
 
-
-
-
-
-
-
-def UrlData(url):
-    r = [-1, 1, 1, 1, -1, -1, -1, -1, -1, 1, 1, -1, 1, -1, 1, -
-         1, -1, -1, 0, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1]
-    return r
-
-
-if __name__ == "__main__":
-    #print(Check_having_IP_Adress("dr.dk"))
-    #print(Check_having_IP_Adress("192.168.40.11"))
-
-#    print(Check_having_IP_Address(fakeURL))
-#    print(Check_having_IP_Address(fakePhishing))
-#    print(Check_URL_Length(fakeURL))
-#    print(Check_URL_Length(fakePhishing))
-#    print(Check_Shortining_Service(fakeURL))
-#    print(Check_Shortining_Service(fakePhishing))
-#    print(Check_Having_At_symbol(fakeURL))
-#    print(Check_Having_At_symbol(fakePhishing))
-#    print(Check_double_slash_redirecting(fakeURL))
-#    print(Check_double_slash_redirecting(fakePhishing))
-#    print(Check_Prefix_Suffix(fakeURL))
-#    print(Check_Prefix_Suffix(fakePhishing))
-#    print(Check_having_Sub_Domain(fakeURL))
-#    print(Check_having_Sub_Domain(fakePhishing))
-#    print(Check_SSLfinal_State(fakeURL))
-#    print(Check_SSLfinal_State(fakePhishing))
-    print(Check_Domain_registeration_length(fakeURL))
-    print(Check_Domain_registeration_length(fakePhishing))
-#    print(Check_Favicon(fakeURL))
-#    print(Check_Favicon(fakePhishing))
-#    print(Check_port(fakeURL))
-#    print(Check_port(fakePhishing))
-#    print(Check_HTTPS_token(fakeURL))
-#    print(Check_HTTPS_token(fakePhishing))
-#    print(Check_Request_URL(fakeURL))
-#    print(Check_Request_URL(fakePhishing))
-#    print(Check_URL_of_Anchor(fakeURL))
-#    print(Check_URL_of_Anchor(fakePhishing))
-#    print(Check_Links_in_tags(fakeURL))
-#    print(Check_Links_in_tags(fakePhishing))
-#    print(Check_SFH(fakeURL))
-#    print(Check_SFH(fakePhishing))
-#    print(Check_Submitting_to_email(fakeURL))
-#    print(Check_Submitting_to_email(fakePhishing))
-#    print(Check_Abnormal_URL(fakeURL))
-#    print(Check_Abnormal_URL(fakePhishing))
-#    print(Check_Redirect(fakeURL))
-#    print(Check_Redirect(fakePhishing))
-#    print(Check_on_mouseover(fakeURL))
-#    print(Check_on_mouseover(fakePhishing))
-#    print(Check_RightClick(fakeURL))
-#    print(Check_RightClick(fakePhishing))
-#    print(Check_popUpWindow(fakeURL))
-#    print(Check_popUpWindow(fakePhishing))
-#    print(Check_Iframe(fakeURL))
-#    print(Check_Iframe(fakePhishing))
-#    print(Check_age_of_domain(fakeURL))
-#    print(Check_age_of_domain(fakePhishing))
-#    print(Check_DNSRecord(fakeURL))
-#    print(Check_DNSRecord(fakePhishing))
-#    print(Check_web_traffic(fakeURL))
-#    print(Check_web_traffic(fakePhishing))
-#    print(Check_Page_Rank(fakeURL))
-#    print(Check_Page_Rank(fakePhishing))
-#    print(Check_Google_Index(fakeURL))
-#    print(Check_Google_Index(fakePhishing))
-#    print(Check_Links_pointing_to_page(fakeURL))
-#    print(Check_Links_pointing_to_page(fakePhishing))
-#    print(Check_Statistical_report(fakeURL))
-#    print(Check_Statistical_report(fakePhishing))
-#    print(Check_Result(fakeURL))
-#    print(Check_Result(fakePhishing))
+#husk ved externe kald, brug "try catch"
