0daafe321fc2d8d1c8cfe46af35c4e84a0b63125
Michael Klan <michaelsanderklan@gmail.com>
2021-01-24 13:56:59 +0100

    Delete Phishing-1 directory

--- Phishing-1/tt.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-a = [-1, 1, -1, -1, 1, 1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 0, 1, -1, -1, -1, -1, -1, 1, 1, -1, 1, 1, 1, -1, -1]
-print(a)
-# b = np.array(a).reshape(1, -1)
-b = np.array(a)
-print(b)
